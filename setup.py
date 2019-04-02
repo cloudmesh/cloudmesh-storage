4419acafd501caf4dfdcc3831068be44fc0fafe0
Gregor von Laszewski <laszewski@gmail.com>
2019-04-02 13:45:58 -0400

    trivial dependency

--- setup.py
+++ setup.py
@@ -28,8 +28,7 @@ def readfile(filename):
 # add minimum requirements here
 #
 requiers = """
-psutil
-pygments
+requests
 """.split("\n")
 
 # dependency_links = ['http://github.com/nicolaiarocci/eve.git@develop']
