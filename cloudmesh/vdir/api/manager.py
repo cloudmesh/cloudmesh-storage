fa721ede11d0c26ddad68142f6859dfd5b03ead6
Gregor von Laszewski <laszewski@gmail.com>
2019-04-02 16:50:58 -0400

    add vdir manager placeholder

--- cloudmesh/vdir/api/manager.py
+++ cloudmesh/vdir/api/manager.py
@@ -1,7 +1,16 @@
-class Manager(object):
+#
+# this manager stors directk=ly into the db wit Database update
+
+from cloudmesh.mongo.DataBaseDecorator import DatabaseUpdate
+
+class Vdir(object):
 
     def __init__(self):
         print("init {name}".format(name=self.__class__.__name__))
+
+    @DatabaseUpdate
+    def mkdir(self):
+        pass
 
     def list(self, parameter):
         print("list", parameter)
