a69087a04570799e73791dd7dc01d0e82893ef3e
Gregor von Laszewski <laszewski@gmail.com>
2019-04-04 18:48:45 -0400

    add spec dit

---
+++ cloudmesh/storage/spec/cloudmesh/storage.py
@@ -0,0 +1,16 @@
+
+
+def get(storage, source, destinatiion, recursive):
+  '''Returns all users
+  '''
+
+  from cloudmesh.storage.Provider import Provider
+
+  provider = Provider(storage)
+
+            result = provider.get(storage,
+                                  SOURCE,
+                                  DESTINATION,
+                                  recursive)
+
+  return josonify(result)
