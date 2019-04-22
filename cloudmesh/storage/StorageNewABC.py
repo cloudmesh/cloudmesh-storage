780377acf2966cb89decf9dee4143c6100ece760
Gregor von Laszewski <laszewski@gmail.com>
2019-04-22 01:42:41 -0400

    add storage abc without service parameter

---
+++ cloudmesh/storage/StorageNewABC.py
@@ -0,0 +1,101 @@
+from abc import ABCMeta
+import yaml
+
+from cloudmesh.management.configuration.config import Config
+from cloudmesh.DEBUG import VERBOSE
+
+
+# noinspection PyUnusedLocal
+class StorageABC(metaclass=ABCMeta):
+
+    def __init__(self, service=None, config="~/.cloudmesh/cloudmesh4.yaml"):
+        try:
+            self.config = Config(config_path=config)
+
+            spec = self.config["cloudmesh.storage"]
+            self.credentials = spec[service]['credentials']
+            self.kind = spec[service]['cm']['kind']
+            self.cloud = service
+            self.service = service
+        except Exception  as e:
+            raise ValueError(f"storage service {service} not specified")
+            print (e)
+
+
+    def create_dir(self,
+                   service=None,
+                   directory=None):
+        """
+        creates a directory
+
+        :param service: the name of the service in the yaml file
+        :param directory: the name of the directory
+        :return: dict
+        """
+        raise NotImplementedError
+        return {}
+
+    def list(self, source=None, recursive=False):
+        """
+        lists the information as dict
+
+        :param source: the source which either can be a directory or file
+        :param recursive: in case of directory the recursive referes to all
+                          subdirectories in the specified source
+        :return: dict
+        """
+        raise NotImplementedError
+        return []
+
+    def put(self, source=None, destination=None, recusrive=False):
+        """
+        puts the source on the service
+
+        :param source: the source which either can be a directory or file
+        :param destination: the destination which either can be a directory or
+                            file
+        :param recursive: in case of directory the recursive referes to all
+                          subdirectories in the specified source
+        :return: dict
+        """
+        raise NotImplementedError
+        return []
+
+    def get(self, source=None, destination=None, recusrive=False):
+        """
+        gets the destination and copies it in source
+
+        :param source: the source which either can be a directory or file
+        :param destination: the destination which either can be a directory or
+                            file
+        :param recursive: in case of directory the recursive referes to all
+                          subdirectories in the specified source
+        :return: dict
+        """
+        raise NotImplementedError
+        return []
+
+    def delete(self, source=None, recusrive=False):
+        """
+        deletes the source
+
+        :param source: the source which either can be a directory or file
+        :param recursive: in case of directory the recursive referes to all
+                          subdirectories in the specified source
+        :return: dict
+        """
+        raise NotImplementedError
+        return []
+
+    def search(self, directory=None, filename=None,
+               recusrive=False):
+        """
+        gets the destination and copies it in source
+
+        :param directory: the directory which either can be a directory or file
+        :param recursive: in case of directory the recursive referes to all
+                          subdirectories in the specified source
+        :return: dict
+        """
+        raise NotImplementedError
+        return []
