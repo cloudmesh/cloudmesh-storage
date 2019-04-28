074880b4df116aa133c16f89153e736cd260fea6
manjunathsivan <46654189+manjunathsivan@users.noreply.github.com>
2019-04-27 23:18:23 -0400

    Added benchmark to awss3

--- tests/test_storage_awss3.py
+++ tests/test_storage_awss3.py
@@ -1,8 +1,8 @@
-###############################################################
+##############################################################################
 # pytest -v --capture=no tests/test_storage_awss3.py
 # pytest -v  tests/test_storage_awss3.py
 # pytest -v --capture=no -v --nocapture tests/test_storage_awss3.py:Test_storage_awss3.<METHIDNAME>
-###############################################################
+################################################################################
 import os
 from pprint import pprint
 
