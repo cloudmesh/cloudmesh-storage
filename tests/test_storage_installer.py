848cb175061cdc3827675d18252db7b7e56455d7
Gregor von Laszewski <laszewski@gmail.com>
2020-03-06 10:29:42 -0500

    switch to Shell.run

--- tests/test_storage_installer.py
+++ tests/test_storage_installer.py
@@ -8,7 +8,7 @@ import os
 import shutil
 
 import pytest
-from cloudmesh_installer.install.util import run
+from cloudmesh.common.Shell import Shell
 
 
 @pytest.mark.incremental
@@ -27,19 +27,19 @@ class TestStorageInstaller:
 
     def test_info(self):
         cmd = "cloudmesh-installer info"
-        result = run(cmd)
+        result = Shell.run(cmd)
         print(result)
         assert "Package" in str(result)
 
     def test_clone_cloud(self):
         cmd = "cd tmp; cloudmesh-installer git clone storage"
-        result = run(cmd)
+        result = Shell.run(cmd)
         print(result)
         assert os.path.isdir("tmp/cloudmesh-cmd5")
 
     def test_install_cms(self):
         cmd = "cd tmp; cloudmesh-installer install storage -e"
-        result = run(cmd)
+        result = Shell.run(cmd)
         print(result)
         assert os.path.isdir("tmp/cloudmesh-cmd5/cloudmesh_cmd5.egg-info")
         assert os.path.isdir("tmp/cloudmesh-cloud/cloudmesh_cloud.egg-info")
@@ -47,7 +47,7 @@ class TestStorageInstaller:
 
     def test_cms_help(self):
         cmd = "cms help"
-        result = run(cmd)
+        result = Shell.run(cmd)
         print(result)
         assert "quit" in result
         assert "storage" in result
@@ -55,7 +55,7 @@ class TestStorageInstaller:
 
     def test_cms_info(self):
         cmd = "cms info"
-        result = run(cmd)
+        result = Shell.run(cmd)
         print(result)
         assert "cloudmesh.common" in result
         assert "cloudmesh.cloud" in result
@@ -63,7 +63,7 @@ class TestStorageInstaller:
 
     def test_cms_version(self):
         cmd = "cms version"
-        result = run(cmd)
+        result = Shell.run(cmd)
         print(result)
         assert "cloudmesh.common" in result
         assert "cloudmesh.cloud" in result
