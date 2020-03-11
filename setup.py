6129583757ba4eab57a2037236fd67f525f62001
Gregor von Laszewski <laszewski@gmail.com>
2020-03-11 10:29:50 -0400

    switch from PRODCUTION to TESTING

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ cloudmesh-abstract
 cloudmesh-configuration
 """.splitlines()
 
-if  "PRODUCTION" not in os.environ:
+if "TESTING" not in os.environ:
     requiers = requiers + requiers_cloudmesh
 
 version = readfile("VERSION")[0].strip()
