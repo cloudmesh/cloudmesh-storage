d58a7a83b0aeb6f7046b31a8f2d79f1b712f26af
Sara Lam <saylam@iu.edu>
2020-03-30 22:18:54 -0500

    test 3

--- test_gdrive2.py
+++ test_gdrive2.py
@@ -1,9 +1,9 @@
 #test_gdrive.py
 from cloudmesh.storage.provider.parallelgdrive.Provider import Provider
 p = Provider(service='parallelgdrive', config="~/.cloudmesh/cloudmesh.yaml")
-p.storage.service
+print(p.storage.service)
 #print(p.create_dir())
 #print(p.list())
-p.get_credentials()
+print(p.get_credentials())
 #print
 #print
