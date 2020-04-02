46a7e5c2cc43c042cd5dee0f811f89ae47c02818
Gregor von Laszewski <laszewski@gmail.com>
2020-04-02 13:11:15 -0400

    Directories moved: project/project_code/cloudmesh-storage_service

--- cloudmesh-exercises/E.Cloudmesh.Common.2.py
+++
@@ -1,9 +0,0 @@
-#E.Cloudmesh.Common.2
-#Demostrate usage of dotdict
-from cloudmesh.common.dotdict import dotdict
-
-dictValues = {"name": "Pratibha", "topic": "Cloudmesh API", "assignment": "E.Cloudmesh.Common.2"}
-
-dictValues = dotdict(dictValues)
-
-print(dictValues.name + "\n" + dictValues.topic + "\n" + dictValues.assignment)
