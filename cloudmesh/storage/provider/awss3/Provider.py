0146fd831a3fd26f930c08233fd064b2545e0ce1
Gregor von Laszewski <laszewski@gmail.com>
2020-04-18 14:57:58 -0400

    sync with master

--- cloudmesh/storage/provider/awss3/Provider.py
+++ cloudmesh/storage/provider/awss3/Provider.py
@@ -41,7 +41,7 @@ class Provider(StorageABC):
     output = {} # "TODO: missing"
 
 
-    def __init__(self, service=None, config="~/.cloudmesh/cloudmesh.yaml"):
+    def __init__(self, service=None):
         """
         TBD
 
@@ -49,7 +49,7 @@ class Provider(StorageABC):
         :param config: TBD
         """
         # pprint(service)
-        super().__init__(service=service, config=config)
+        super().__init__(service=service)
         self.container_name = self.credentials['bucket']
         self.s3_resource = boto3.resource(
             's3',
--- cloudmesh/storage/provider/box/Provider.py
+++ cloudmesh/storage/provider/box/Provider.py
@@ -1,11 +1,10 @@
-import os
-from os.path import basename, join, dirname
-
-from boxsdk import Client
 from boxsdk import JWTAuth
-from cloudmesh.abstract.StorageDeprecatedABC import StorageABC
+from boxsdk import Client
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
+from os.path import basename, join, dirname
+import os
+from cloudmesh.abstract.StorageABC import StorageABC
 
 
 def get_id(source, results, source_type):
@@ -70,11 +69,9 @@ class Provider(StorageABC):
 
     output = {}  # "TODO: missing"
 
-    def __init__(self,
-                 service=None,
-                 config="~/.cloudmesh/cloudmesh.yaml"):
+    def __init__(self, service=None):
 
-        super().__init__(service=service, config=config)
+        super().__init__(service=service)
         self.sdk = JWTAuth.from_settings_file(self.credentials['config_path'])
         self.client = Client(self.sdk)
 
